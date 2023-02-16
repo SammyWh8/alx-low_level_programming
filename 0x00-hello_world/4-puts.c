3df0665ad56ac60e83f5a8a7d91ab7a70c036912
Samantha <samanthadlamini1@gmail.com>
2023-02-16 10:32:16 -0800

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,10 @@
  * main - Entry
  * Return: Always 0
  */
-
 int main(void)	
 {
-	puts("\"Programming is like building a multi              lingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }	
 
 
