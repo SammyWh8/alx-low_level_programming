c2e8d552f5d97b4968ec51dbe1556da4bbc45b47
Samantha <samanthadlamini1@gmail.com>
2023-02-19 04:05:14 -0800

    print alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,19 +2,16 @@
 /**
  * main - Entry Point
  *
- * Return; Always 0
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char c;
 
-	c = 'a';
-	while
-		(c <= 'z') {
-			putchar(c);
-			c++;
-		}
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
 	putchar('\n');
+
 	return (0);
 }
